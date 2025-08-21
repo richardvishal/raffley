437057c2b19849e0e60855b2df7649f0b89c8a77
richie <richie@ardhika.com>
2025-08-21 18:55:22 +0530

    Added some js commands

--- lib/raffley_web/controllers/live/admin_raffle_live/index.ex
+++ lib/raffley_web/controllers/live/admin_raffle_live/index.ex
@@ -14,14 +14,35 @@ defmodule RaffleyWeb.AdminRaffleLive.Index do
   def render(assigns) do
     ~H"""
     <div class="admin-index">
-      <.header>
+      <.button phx-click={
+        JS.toggle(
+          to: "#joke",
+          # 1 -> css transition classes
+          # 2 -> starting transition classes
+          # 3 -> ending transition classes
+          in: {"ease-in-out duration-300", "opacity-0", "opacity-100"},
+          out: {"ease-in-out duration-300", "opacity-100", "opacity-0"},
+          # default -> 200
+          time: 300
+        )
+      }>
+        Toggle Joke
+      </.button>
+      <div id="joke" class="joke hidden">
+        What's the tree's favorite drink?
+      </div>
+      <.header class="mt-6 ">
         {@page_title}
         <:actions>
           <.link class="button" navigate={~p"/admin/raffles/new"}>New Raffle</.link>
         </:actions>
       </.header>
 
-      <.table id="raffles" rows={@streams.raffles}>
+      <.table
+        id="raffles"
+        rows={@streams.raffles}
+        row_click={fn {_dom_id, raffle} -> JS.navigate(~p"/raffles/#{raffle}") end}
+      >
         <:col :let={{_dom_id, raffle}} label="Prize">
           <.link navigate={~p"/raffles/#{raffle}"}>{raffle.prize}</.link>
         </:col>
@@ -32,8 +53,10 @@ defmodule RaffleyWeb.AdminRaffleLive.Index do
         <:col :let={{_dom_id, raffle}} label="Ticket Price">{raffle.ticket_price}</:col>
         <:action :let={{_dom_id, raffle}}>
           <.link navigate={~p"/admin/raffles/#{raffle}/edit"}>Edit</.link>
-          <.link phx-click="delete" phx-value-id={raffle.id} data-confirm="Are you sure?">
-            Delete
+        </:action>
+        <:action :let={{dom_id, raffle}}>
+          <.link phx-click={delete_and_hide(dom_id, raffle)} data-confirm="Are you sure?">
+            <.icon name="hero-trash" class="h-4 w-4" /> Delete
           </.link>
         </:action>
       </.table>
@@ -45,5 +68,10 @@ defmodule RaffleyWeb.AdminRaffleLive.Index do
     raffle = Admin.get_raffle!(id)
     {:ok, _} = Admin.delete_raffle(raffle)
     {:noreply, stream_delete(socket, :raffles, raffle)}
+  end
+
+  defp delete_and_hide(dom_id, raffle) do
+    JS.push("delete", value: %{id: raffle.id})
+    |> JS.hide(to: "##{dom_id}", transition: "fade-out")
   end
 end
